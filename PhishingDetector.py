f1779857bae76c0ebf9257e57295e5cdc0ee7758
Harsh Khatri <31386281+Ferreter@users.noreply.github.com>
2023-11-27 03:37:35 +0000

    Added support for Language
    Added Fr and Hindi Support

--- PhishingDetector.py
+++ PhishingDetector.py
@@ -1,104 +1,206 @@
+
+#'AIzaSyBsO6OGeF6ZXsqPFwEsSWIsOYPjHIKoGpQ'
+#6635501788:AAHEEP3hFf57zLZfH-XsqvcjNcRuzep0e_0
+
+
+
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import requests
 import re
 
-def check_numbers_in_uri(uri):
+# Define language-specific message templates
 
-    # Regular expression to match any digit (0-9) in the URI
+EN_MESSAGES = {
+    "greeting": "Hello {first_name}",
+    "provide_url": "Please provide a URL to check. Use /check URL_GOES_HERE",
+    "malware_alert": "Alert: The URL is identified as MALWARE. Malware can harm your device. Avoid clicking on the link to protect your data and device.",
+    "social_engineering_caution": "Caution: The URL is identified as a SOCIAL ENGINEERING threat. Social engineering attempts to manipulate you. Do not proceed to the website and report the link.",
+    "unwanted_software_warning": "Warning: The URL is identified as UNWANTED SOFTWARE. Unwanted software can be harmful. Avoid interacting with the link to ensure your device's security.",
+    "social_engineering_extended_caution": "Attention: The URL is identified as a SOCIAL ENGINEERING (Extended Coverage) threat. Be cautious and avoid the website. Report the link to prevent potential harm.",
+    "no_threat_info": "No threat information available. The URL is not on any threat lists. Proceed with caution.\n\nRemember: New threats are created every day, and even though Google's Webrisk list is regularly updated, it is still possible to be a threat.",
+    "uri_contains_numbers": "Caution: The URI contains numbers. When checking links, be mindful of unusual elements like numbers in the URL. Links with numbers might indicate suspicious activity. Exercise caution and consider verifying the source before clicking such links.",
+    "uri_safe": "Tip: When checking links, pay attention to the structure of the URL. Unusual elements or unexpected characters may indicate potential risks. The current URI is free from numbers, but it's always good practice to stay vigilant when navigating the web.",
+    # Add more English messages here
+}
+
+FR_MESSAGES = {
+    "greeting": "Bonjour {first_name}",
+    "provide_url": "Veuillez fournir une URL à vérifier. Utilisez /check URL_ICI",
+    "malware_alert": "Alerte : L'URL est identifiée comme MALVEILLANTE. Les logiciels malveillants peuvent endommager votre appareil. Évitez de cliquer sur le lien pour protéger vos données et votre appareil.",
+    "social_engineering_caution": "Mise en garde : L'URL est identifiée comme une menace d'INGÉNIERIE SOCIALE. L'ingénierie sociale tente de vous manipuler. Ne continuez pas vers le site web et signalez le lien.",
+    "unwanted_software_warning": "Avertissement : L'URL est identifiée comme LOGICIEL NON SOUHAITÉ. Les logiciels non souhaités peuvent être nuisibles. Évitez d'interagir avec le lien pour assurer la sécurité de votre appareil.",
+    "social_engineering_extended_caution": "Attention : L'URL est identifiée comme une menace d'INGÉNIERIE SOCIALE (couverture étendue). Soyez prudent et évitez le site web. Signalez le lien pour éviter tout dommage potentiel.",
+    "no_threat_info": "Aucune information sur la menace n'est disponible. L'URL ne figure sur aucune liste de menaces. Procédez avec prudence.\n\nN'oubliez pas : de nouvelles menaces sont créées chaque jour, et même si la liste Webrisk de Google est régulièrement mise à jour, il est toujours possible qu'elle soit une menace.",
+    "uri_contains_numbers": "Mise en garde : L'URI contient des chiffres. Lors de la vérification des liens, soyez attentif aux éléments inhabituels tels que les chiffres dans l'URL. Les liens contenant des chiffres peuvent indiquer une activité suspecte. Faites preuve de prudence et envisagez de vérifier la source avant de cliquer sur de tels liens.",
+    "uri_safe": "Conseil : Lors de la vérification des liens, soyez attentif à la structure de l'URL. Des éléments inhabituels ou des caractères inattendus peuvent indiquer des risques potentiels. L'URI actuel ne contient pas de chiffres, mais il est toujours bon de rester vigilant lors de la navigation sur le Web.",
+    # Add more French messages here
+}
+
+HI_MESSAGES = {
+    "greeting": "नमस्ते {first_name}",
+    "provide_url": "कृपया जांच करने के लिए एक URL प्रदान करें। /check URL_GOES_HERE का उपयोग करें",
+    "malware_alert": "चेतावनी: URL को MALWARE के रूप में पहचाना गया है। मैलवेयर आपके डिवाइस को हानि पहुंचा सकता है। अपने डेटा और डिवाइस की सुरक्षा के लिए लिंक पर क्लिक करने से बचें।",
+    "social_engineering_caution": "सतर्कता: URL को सामाजिक इंजीनियरिंग खतरा के रूप में पहचाना गया है। सामाजिक इंजीनियरिंग आपको मानव त्रुटि का उपयोग करके मनिपुरित करने का प्रयास करता है। वेबसाइट पर आगे न बढ़ें और लिंक की सूचना दें।",
+    "unwanted_software_warning": "चेतावनी: URL को अवांछित सॉफ़्टवेयर के रूप में पहचाना गया है। अवांछित सॉफ़्टवेयर हानिकारक हो सकता है। अपने डिवाइस की सुरक्षा सुनिश्चित करने के लिए लिंक के साथ बातचीत न करें।",
+    "social_engineering_extended_caution": "ध्यान दें: URL को सामाजिक इंजीनियरिंग (विस्तारित कवरेज) खतरा के रूप में पहचाना गया है। सतर्क रहें और वेबसाइट से बचें। पोटेंशियल हानि से बचाने के लिए लिंक की सूचना दें।",
+    "no_threat_info": "कोई खतरा सूचना उपलब्ध नहीं है। URL को किसी भी खतरे की सूची पर नहीं रखा गया है। सतर्कता के साथ आगे बढ़ें।\n\nध्यान दें: नई खतरों को रोज़ बनाया जाता है, और यह भी सच है कि गूगल की वेबरिस्क सूची को नियमित रूप से अपडेट किया जाता है, फिर भी यह एक खतरा हो सकता है।",
+    "uri_contains_numbers": "सतर्कता: URI में नंबर्स हैं। लिंक जाँचते समय URI में नंबर्स जैसे असामान्य तत्वों के प्रति सतर्क रहें। नंबर्स वाले लिंक संदेहपूर्ण गतिविधि की सूचना देने के रूप में हो सकते हैं। सतर्क रहें और इस तरह के लिंक पर क्लिक करने से पहले स्रोत की सत्यापन करने का विचार करें।",
+    "uri_safe": "सुझाव: लिंक जाँचते समय URL की संरचना पर ध्यान दें। असामान्य तत्वों या अप्रत्याशित अक्षरों की ज्यादा वर्गीकरण संभावित खतरों की सूचना दे सकते हैं। मौजूदा URI में नंबर्स नहीं हैं, लेकिन वेब नेविगेशन करते समय सतर्क रहना हमेशा अच्छा अभ्यास है।",
+    # Add more Hindi messages here
+}
+
+# Create a variable to store the current language
+current_language = "en"  # Default to English
+
+# Modify the check_numbers_in_uri function to use language-specific messages
+def check_numbers_in_uri(uri):
     digit_pattern = re.compile(r'\d')
 
     if digit_pattern.search(uri):
         return (
-            "Caution: The URI contains numbers. "
-            "When checking links, be mindful of unusual elements like numbers in the URL. "
-            "Links with numbers might indicate suspicious activity. "
-            "Exercise caution and consider verifying the source before clicking such links."
+            EN_MESSAGES["uri_contains_numbers"]
+            if current_language == "en"
+            else FR_MESSAGES["uri_contains_numbers"]
+            if current_language == "fr"
+            else HI_MESSAGES["uri_contains_numbers"]
         )
     else:
         return (
-            "Tip: When checking links, pay attention to the structure of the URL. "
-            "Unusual elements or unexpected characters may indicate potential risks. "
-            "The current URI is free from numbers, but it's always good practice to stay vigilant when navigating the web."
+            EN_MESSAGES["uri_safe"]
+            if current_language == "en"
+            else FR_MESSAGES["uri_safe"]
+            if current_language == "fr"
+            else HI_MESSAGES["uri_safe"]
         )
 
+# Modify the /language command handler to allow users to switch languages
+async def set_language(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    global current_language
+    text = context.args[0].lower() if context.args else ""
+    
+    if text == "fr":
+        current_language = "fr"
+    elif text == "hi":
+        current_language = "hi"
+    else:
+        current_language = "en"
+    
+    await update.message.reply_text("Language set to " + current_language.upper())
+
+# Modify the hello function to use language-specific messages
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user = update.effective_user
+    greeting_message = EN_MESSAGES["greeting"] if current_language == "en" else FR_MESSAGES["greeting"] if current_language == "fr" else HI_MESSAGES["greeting"]
+    await update.message.reply_text(greeting_message.format(first_name=user.first_name))
+
+# Modify the check_url function to use language-specific messages
+async def check_url(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user = update.effective_user
+    url_to_check = context.args[0] if context.args else None
+
+    if url_to_check:
+        response_message = check_url_threat(url_to_check, api_key)
+    else:
+        response_message = (
+            EN_MESSAGES["provide_url"]
+            if current_language == "en"
+            else FR_MESSAGES["provide_url"]
+            if current_language == "fr"
+            else HI_MESSAGES["provide_url"]
+        )
+
+    await update.message.reply_text(response_message)
+
+# Modify the check_url_threat function to use language-specific messages
 def check_url_threat(uri, api_key):
-    # API url
     url = 'https://webrisk.googleapis.com/v1/uris:search'
 
-    #All threat types in webrisk
     params = {
         'threatTypes': ['SOCIAL_ENGINEERING', 'MALWARE', 'UNWANTED_SOFTWARE', 'SOCIAL_ENGINEERING_EXTENDED_COVERAGE'],
         'uri': uri,
         'key': api_key
     }
 
-    # Send the GET request
     response = requests.get(url, params=params)
 
-    # Check the response status
     if response.status_code == 200:
-        # Successful request
         data = response.json()
         
-        # Check if the threat is present
         if 'threat' in data:
             threat_types = data['threat']['threatTypes']
             expire_time = data['threat']['expireTime']
 
-            print(f"Threat Types: {threat_types}")
-            print(f"Expire Time: {expire_time}")
-
             response_message = ""
 
-            # Check for specific threat types
             if 'MALWARE' in threat_types:
-                response_message += "Alert: The URL is identified as MALWARE. Malware can harm your device. Avoid clicking on the link to protect your data and device."
+                response_message += (
+                    EN_MESSAGES["malware_alert"]
+                    if current_language == "en"
+                    else FR_MESSAGES["malware_alert"]
+                    if current_language == "fr"
+                    else HI_MESSAGES["malware_alert"]
+                )
                 response_message += "\n"+check_numbers_in_uri(uri)
             elif 'SOCIAL_ENGINEERING' in threat_types:
-                response_message += "Caution: The URL is identified as a SOCIAL ENGINEERING threat. Social engineering attempts to manipulate you. Do not proceed to the website and report the link."
+                response_message += (
+                    EN_MESSAGES["social_engineering_caution"]
+                    if current_language == "en"
+                    else FR_MESSAGES["social_engineering_caution"]
+                    if current_language == "fr"
+                    else HI_MESSAGES["social_engineering_caution"]
+                )
                 response_message += "\n\n Social engineering is a manipulation technique that exploits human error to gain private information, access, or valuables.\n"
                 response_message += "\n"+check_numbers_in_uri(uri)
             elif 'UNWANTED_SOFTWARE' in threat_types:
-                response_message += "Warning: The URL is identified as UNWANTED SOFTWARE. Unwanted software can be harmful. Avoid interacting with the link to ensure your device's security."
+                response_message += (
+                    EN_MESSAGES["unwanted_software_warning"]
+                    if current_language == "en"
+                    else FR_MESSAGES["unwanted_software_warning"]
+                    if current_language == "fr"
+                    else HI_MESSAGES["unwanted_software_warning"]
+                )
                 response_message += "\n"+check_numbers_in_uri(uri)
             elif 'SOCIAL_ENGINEERING_EXTENDED_COVERAGE' in threat_types:
-                response_message += "Attention: The URL is identified as a SOCIAL ENGINEERING (Extended Coverage) threat. Be cautious and avoid the website. Report the link to prevent potential harm."
+                response_message += (
+                    EN_MESSAGES["social_engineering_extended_caution"]
+                    if current_language == "en"
+                    else FR_MESSAGES["social_engineering_extended_caution"]
+                    if current_language == "fr"
+                    else HI_MESSAGES["social_engineering_extended_caution"]
+                )
                 response_message += "\n\n Social engineering is a manipulation technique that exploits human error to gain private information, access, or valuables.\n"
                 response_message += "\n"+check_numbers_in_uri(uri)
             else:
-                response_message += "The URL is safe from known threats."
+                response_message += (
+                    EN_MESSAGES["no_threat_info"]
+                    if current_language == "en"
+                    else FR_MESSAGES["no_threat_info"]
+                    if current_language == "fr"
+                    else HI_MESSAGES["no_threat_info"]
+                )
 
             return response_message
         else:
-            return "No threat information available. The URL is not on any threat lists.Proceed with caution\n\n\nRemember: New threats are created every day and even though Googles Webrisk list is regularly updated it is still possible to be threat"
+            return (
+                EN_MESSAGES["no_threat_info"]
+                if current_language == "en"
+                else FR_MESSAGES["no_threat_info"]
+                if current_language == "fr"
+                else HI_MESSAGES["no_threat_info"]
+            )
     else:
-        # Error handling
         return f"Error: {response.status_code}\n{response.text}"
 
-
-#webrisk API
+# Define your Webrisk API key
 api_key = 'AIzaSyBsO6OGeF6ZXsqPFwEsSWIsOYPjHIKoGpQ'
 
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
-    
-    
-async def check_url(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    # Get the URL from the command parameters
-    url_to_check = context.args[0] if context.args else None
-
-    if url_to_check:
-        response_message = check_url_threat(url_to_check, api_key)
-        await update.message.reply_text(response_message)
-    else:
-        await update.message.reply_text('Please provide a URL to check. Use /check URL_GOES_HERE')
-
-
-#telegram bot api key goes here 
+# Create the application and add handlers
 app = ApplicationBuilder().token("6635501788:AAHEEP3hFf57zLZfH-XsqvcjNcRuzep0e_0").build()
 
 app.add_handler(CommandHandler("hello", hello))
 app.add_handler(CommandHandler("check", check_url))
+app.add_handler(CommandHandler("language", set_language))
 
+# Run the bot
 app.run_polling()
